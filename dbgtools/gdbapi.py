67e37d994051588b62cf4f7e38d571b6c07fec41
ju256 <32434661+ju256@users.noreply.github.com>
2024-02-13 21:58:21 +0100

    memory examination shortcut during debug

--- dbgtools/gdbapi.py
+++ dbgtools/gdbapi.py
@@ -1,13 +1,14 @@
-from typing import Optional
+from typing import Optional, Sequence
 import gdb
+import re
+import pwndbg
 
-
-def execute_commands(cmds: list[str]):
+def execute_commands(cmds: list[str]) -> Sequence[str]:
     for cmd in cmds:
-        gdb.execute(cmd, to_string=True)
+        yield execute_command(cmd)
 
-def execute_command(cmd: str):
-    execute_commands([cmd])
+def execute_command(cmd: str) -> str:
+    return gdb.execute(cmd, to_string=True)
 
 def set_breakpoint(addr: int):
     execute_command(f"b *{hex(addr)}")
@@ -36,3 +37,17 @@ def parse_and_eval(s: str):
 
 def finish():
     execute_command("finish")
+
+def examine(ptr: int, repeat: int = 8, size: int = 8, fmt: str = "x"):
+    size_fmt_map = {1: 'b', 2: 'h', 4: 'w', 8: 'g'}
+    if size not in size_fmt_map.keys():
+        raise ValueError(f"size has to be in {list(size_fmt.keys())}")
+    size_fmt = size_fmt_map[size]
+    data = execute_command(f"x/{repeat}{size_fmt}{fmt} {hex(ptr)}")
+    if size == 8:
+        # color potential pointers
+        matches = re.findall(r"(0x[a-z0-9]{8,16})", data)
+        for q in matches:
+            colored = pwndbg.color.memory.get(int(q, 16), q)
+            data = data.replace(q, colored)
+    return data
